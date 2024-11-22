7275523a2d9e68ee3da99afd5d707e3622873ca0
Sem Lyakin <xsemx@yandex.ru>
2024-11-22 22:36:25 +0400

    Сборка проекта

--- main-service/src/main/java/ru/practicum/ExploreWithMeApp.java
+++ main-service/src/main/java/ru/practicum/ExploreWithMeApp.java
@@ -3,7 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum"})
+@SpringBootApplication
 public class ExploreWithMeApp {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeApp.class, args);
