e69276be397d840be38d4e55f461692a24948b0d
Sem Lyakin <xsemx@yandex.ru>
2024-11-23 17:06:13 +0400

    Сборка проекта

--- stats-service/service/src/main/java/ru/practicum/controller/StatController.java
+++ stats-service/service/src/main/java/ru/practicum/controller/StatController.java
@@ -2,7 +2,6 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.BadRequestException;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
